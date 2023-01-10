639187dbe08cdf657367af849bb1aac00058a882
pjnw1236 <pjnw1236@gmail.com>
2023-01-10 21:33:18 +0900

    section11 - JavaScript Basic

---
+++ section11/app.js
@@ -0,0 +1,31 @@
+let age = 32; 
+let userName = 'pjw'; 
+let hobbies = ['Sports', 'Cooking', 'Reading'];
+let job = {
+    title: 'Developer', 
+    place: 'New York', 
+    salary: 50000
+};
+
+let totalAdultYears;
+
+function calculateAdultYears(userAge) {
+    let result;
+    result = userAge - 18;
+    return result;
+}
+
+totalAdultYears = calculateAdultYears(age);
+console.log(totalAdultYears);
+
+age = 45; 
+totalAdultYears = calculateAdultYears(age); 
+
+console.log(totalAdultYears); 
+
+let person = {
+    name: 'Max',                       // Property
+    greet() { console.log('Hello'); }  // Method
+}
+
+person.greet();
